c76f65990cf988d4e0e901dc214a0efa86868bc4
DFW1N <45083490+DFW1N@users.noreply.github.com>
2022-04-04 12:35:46 +1000

    Update kubernetes-automate.sh

--- kubernetes-automate.sh
+++ kubernetes-automate.sh
@@ -50,6 +50,16 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
+echo -e "${WHITE}Install ${GREEN}vscode${NC}." && echo
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code -y
+
 echo -e "${WHITE}Installing ${GREEN}Kubernetes${NC}." && echo
 sudo apt -y install curl apt-transport-https -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
