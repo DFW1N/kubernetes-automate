65b95d2c1ee2c1439f97616b7deab4a1843b0dcf
DFW1N <45083490+DFW1N@users.noreply.github.com>
2022-04-04 13:20:18 +1000

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -3,8 +3,9 @@
 ####################
 
 dockerEnvironment="dockerenv1"
-acr="acr-k8s-training"
-acr_url=""
+dockerName="sql-env"
+acr="registryqjl3186"
+acr_url="$acr.azurecr.io"
 sql_password="Sql@12345"
 
 echo -e "${WHITE}Set up ${GREEN}Docker Environment${NC}." && echo
@@ -12,9 +13,9 @@ echo -e "${WHITE}Set up ${GREEN}Docker Environment${NC}." && echo
 #docker network create --driver=bridge --subnet=192.168.0.0/16 br0
 sudo docker network create $dockerEnvironment
 echo -e "${WHITE}Run ${GREEN}Docker Instance${NC}." && echo
-sudo docker run --name sql --network $dockerEnvironment -p 1433:1433 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sql_password" -d mcr.microsoft.com/mssql/server:2017-latestsu
+sudo docker run --name $dockerName --network $dockerEnvironment -p 1437:1437 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$sql_password" -d mcr.microsoft.com/mssql/server:2017-latest
 echo -e "${WHITE}Checking running ${GREEN}Docker Instances${NC}." && echo
 sudo docker ps
 
-echo -e "${WHITE}Create Database${GREEN}Instance${NC}." && echo
-sudo docker exec -it sql /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $sql_password -Q "CREATE DATABASE [master_db]"
+echo -e "${WHITE}Create Database ${GREEN}Instance${NC}." && echo
+sudo docker exec -it $dockerName /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $sql_password -Q "CREATE DATABASE [master_db]"
